f5bf925914017c7da810b6b565f5d4e58523f642
inokappa <inokara@gmail.com>
2018-11-17 15:34:49 +0900

    fixed ref#12

--- lib/furikake/resource.rb
+++ lib/furikake/resource.rb
@@ -1,19 +1,21 @@
 module Furikake
   class Resource
     def self.generate
-      types = %w(
-        ec2 alb clb lambda elasticsearch_service
-        kinesis rds directory_service vpc vpc_endpoint
-        security_group
-      )
-
       documents = ""
-      types.each do |type|
+      load_type.each do |type|
         require "furikake/resources/#{type}"
         eval "documents.concat(Furikake::Resources::#{type.camelize}.report)"
         documents.concat("\n")
       end
       documents
+    end
+
+    def self.load_type
+      type = []
+      Dir.glob(File.dirname(__FILE__) + '/resources/*').each do |r|
+        type << File.basename(r, '.rb')
+      end
+      type
     end
   end
 end
