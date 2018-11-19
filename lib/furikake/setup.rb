ecf144bde695006e1dc26f6fbdb423704ec3854d
inokappa <inokara@gmail.com>
2018-11-20 07:30:35 +0900

    setup コマンドに addons ディレクトリ作成の処理を追加した

--- lib/furikake/setup.rb
+++ lib/furikake/setup.rb
@@ -4,6 +4,7 @@ module Furikake
       puts '[setup] creating .furikake.yml...'
       create_furikake_yaml unless File.exist?('.furikake.yml')
       puts '[setup] .furikake.yml already exists.'
+      setup_addons
       exit
     end
 
@@ -38,6 +39,57 @@ EOS
         f.write(erb.result(binding))
       end
       puts '[setup] .furikake.yml created.'
+      exit
+    end
+
+    def self.setup_addons
+      puts '[setup] create `addons` directory? [y/n]'
+      response = STDIN.gets.chomp
+      case response
+      when /^[yY]/
+        self.create_addons_directory unless FileTest.exist?('addons')
+        puts '[setup] `addons` directory already exists.'
+        exit
+      when /^[nN]/, /^$/
+        puts '[setup] `addons` directory not created.'
+        exit 0
+      else
+        puts 'Please input y(Y) or n(N)'
+        setup_addons
+      end
+    end
+
+    def self.create_addons_directory
+      puts '[setup] creating `addons` directory.'
+      FileUtils.mkdir_p('addons')
+      f = File.open('addons/furikake-resource-addon-example.rb', 'w')
+      f.print <<EOS
+module Furikake::Resources
+  module Addons
+    class Example
+      def self.report(format = nil)
+        values = [['value1', 'value2'], ['value3', 'value4']]
+        contents = {
+          title: 'Example',
+          resources: [
+            {
+               subtitle: '',
+               header: ['Title1', 'Title2'],
+               resource: values
+            }
+          ]
+        }
+        Furikake::Formatter.shaping(format, contents)
+      end
+
+      def self.get_resources
+        ... Please implement ...
+      end
+    end
+  end
+end
+EOS
+      puts '[setup] `addons` directory created.'
       exit
     end
   end
