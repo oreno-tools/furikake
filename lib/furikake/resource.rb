943e2ec161bf163166ead1d7fdc209321c38fa16
inokappa <inokara@gmail.com>
2018-11-19 21:33:38 +0900

    試験的に addons ディレクトリを追加

--- lib/furikake/resource.rb
+++ lib/furikake/resource.rb
@@ -2,18 +2,30 @@ module Furikake
   class Resource
     def self.generate
       documents = ''
-      load_type.each do |type|
-        require "furikake/resources/#{type}"
-        eval "documents.concat(Furikake::Resources::#{type.camelize}.report)"
-        documents.concat("\n")
+      load_resource_type.each do |type|
+        if type.include?('addon')
+          $LOAD_PATH.push(Dir.pwd + '/addons')
+          require "#{type}"
+          type_name = type.split('-')[-1]
+          eval "documents.concat(Furikake::Resources::Addons::#{type_name.camelize}.report)"
+          documents.concat("\n\n")
+        else
+          require "furikake/resources/#{type}"
+          eval "documents.concat(Furikake::Resources::#{type.camelize}.report)"
+          documents.concat("\n\n")
+        end
       end
       documents
     end
 
-    def self.load_type
+    def self.load_resource_type
       type = []
       Dir.glob(File.dirname(__FILE__) + '/resources/*').each do |r|
         type << File.basename(r, '.rb') unless r.include?('stub')
+      end
+
+      Dir.glob(Dir.pwd + '/addons/furikake-resource-addon-*').each do |r|
+        type << File.basename(r, '.rb')
       end
       type
     end
