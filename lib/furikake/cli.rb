8894186a60b34d7a0e84f211fe8d2be989173a05
inokappa <inokara@gmail.com>
2018-11-23 09:53:40 +0900

    Add sub command for monitor

--- lib/furikake/cli.rb
+++ lib/furikake/cli.rb
@@ -13,10 +13,18 @@ module Furikake
       report.show
     end
 
-    desc 'publish', 'resouces publish to something.'
+    desc 'publish', 'resouces publish to something. (Default: Backlog)'
     def publish
       report = Furikake::Report.new
       report.publish
+    end
+
+    desc 'monitor', 'resouces publish to something by daemonize process.'
+    option :interval, type: :numeric, aliases: '-i', default: 3600, desc: 'specify monitor interval (sec).'
+    option :pid, type: :string, aliases: '-p', default: 'furikake.pid', desc: 'specify PID file path (Default: furikake.pid)'
+    def monitor
+      monitor = Furikake::Monitor.new(options)
+      monitor.run
     end
 
     desc 'setup', 'generate .furikake.yml template.'
